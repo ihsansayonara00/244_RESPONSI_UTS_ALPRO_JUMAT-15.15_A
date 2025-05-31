6c6af77875ebc94b3fb9e85e9577621ec7688798
ihsansayonara00 <rafiihsandi31@gmail.com>
2025-05-31 18:14:22 +0700

    Add files via upload

---
+++ 244_RESPONSI_UTS_ALPRO_JUMAT 15.15_C.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	string user;
+	int password;
+	string nama = "rafi";
+	int nim = 244;
+	bool LOGIN = false;
+	string jenisBarang;
+	
+	
+
+	
+	cout<<"=========LOGIN==========="<<endl;
+	for(int i = 1; i<=1;i++){
+
+	cout<<"|Masukkan username :";cin>>nama;
+	cout<<"|Masukkan password : ";cin>>password;
+	
+	if(user == nama && password == nim){
+		LOGIN = true;
+		int i = 1; 
+		}else{
+		cout<<"username dan password salah"<<endl;
+	}
+}
+}
+	do{
+		cout<<"saldo saat ini : "<<endl;
+		cout<<"jumlah pesanan : "<<endl;
+		cout<<"alamat pemesanan pengiriman"<<endl;
+	}
+	
+	switch(pilihan){
+			
+			case 1:
+				int n = 0
+				cout<<"saldo saat ini"<<n;
+				
+				
+			case 2:
+				int i;
+				cout<<"masukkan jumlah pesanan";
+				cin>>pesanan;
+				for(i=1; i <=0; i==){
+					cout<<"Barang pesanan";cin>>jenisBarang;
+				}
+				
+				case 3:
+					cout<<"alamat tujuan : ";
+					cin>>n;
+					
+					case 4 :
+						cout<<"voucher diskon setiap pembelian 3 lebih barang "<<endl;
+						
+						
+					
+return 0;
+}
+		
+		
+
+	
